84fd1a07a61ebcb36471c305df15b54c613a76b0
bwaidelich <b.waidelich@wwwision.de>
2020-02-07 13:22:25 +0100

    FEATURE: Neos 4.3 compatibility and some tweaks
    * Removes `q` from Eel Context for compatibility with Neos < 5.0
    * Don't add `AnchorMixin` to all `Content` node types
    * Add validation for provided `AnchorMixin.anchor` property
    * Allow `icon` and `group` of resolver result to be configurable
      and provide some sane defaults
    * Turn `AnchorWrapper` prototype into an Augmentor and rename it to
      `AnchorLinkAugmentor` accordingly
    * Some (mostly cosmetic) tweaks

--- Classes/AnchorLinkResolverInterface.php
+++ Classes/AnchorLinkResolverInterface.php
@@ -6,26 +6,28 @@ use Neos\ContentRepository\Domain\Model\NodeInterface;
 
 interface AnchorLinkResolverInterface
 {
-  /**
-   * Return an array of options for the anchor link selectbox:
-   *
-   * [
-   *      [
-   *           'group' => 'first', // optional
-   *           'value' => 'bar',
-   *           'label' => 'Bar',
-   *      ],
-   *      [
-   *           'group' => 'second',
-   *           'value' => 'baz',
-   *           'label' => 'Baz',
-   *      ]
-   * ];
-   *
-   * @param NodeInterface $node Currently focused node
-   * @param string $link Current link value
-   * @param string $searchTerm Search term
-   * @return array
-   */
-  public function resolve(NodeInterface $node, string $link, string $searchTerm);
+    /**
+     * Return an array of options for the anchor link selectbox:
+     *
+     * [
+     *      [
+     *           'icon'  => 'icon-foo', // optional
+     *           'group' => 'first', // optional
+     *           'value' => 'bar',
+     *           'label' => 'Bar',
+     *      ],
+     *      [
+     *           'icon'  => 'icon-foo', // optional
+     *           'group' => 'second',
+     *           'value' => 'baz',
+     *           'label' => 'Baz',
+     *      ]
+     * ];
+     *
+     * @param NodeInterface $node Currently focused node
+     * @param string $link Current link target (for example "node://<some-identifier>" or "https://www.external.url")
+     * @param string $searchTerm Search term (term that has been entered in the "Choose link anchor" search field, defaults to an empty string)
+     * @return array
+     */
+    public function resolve(NodeInterface $node, string $link, string $searchTerm): array;
 }
